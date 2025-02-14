0ffac5eca3af3226ffbdb84190681f9d93344aad
Stanislas123456789 <161655685+Stanislas123456789@users.noreply.github.com>
2025-02-14 09:29:55 -0300

    Update run.py

--- Ligne de l'amour/run.py
+++ Ligne de l'amour/run.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from mon_app import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, this is your Flask app!"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
